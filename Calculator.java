d1c0a91f8f8c87de70aab92bb45f8e149d3cabcb
ankita-pandey06 <pandeyankita1904@gmail.com>
2026-02-19 12:01:06 +0530

    Add files via upload

---
+++ Calculator.java
@@ -0,0 +1,44 @@
+10import java.util.Scanner;
+
+public class Calculator {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter first number:");
+        double num1 = sc.nextDouble();
+
+        System.out.println("Enter second number:");
+        double num2 = sc.nextDouble();
+
+        System.out.println("Choose operation: +  -  *  /");
+        char op = sc.next().charAt(0);
+
+        double result;
+
+        if(op == '+') {
+            result = num1 + num2;
+            System.out.println("Result = " + result);
+        }
+        else if(op == '-') {
+            result = num1 - num2;
+            System.out.println("Result = " + result);
+        }
+        else if(op == '*') {
+            result = num1 * num2;
+            System.out.println("Result = " + result);
+        }
+        else if(op == '/') {
+            if(num2 != 0) {
+                result = num1 / num2;
+                System.out.println("Result = " + result);
+            } else {
+                System.out.println("Cannot divide by zero");
+            }
+        }
+        else {
+            System.out.println("Invalid operator");
+        }
+
+        sc.close();
+    }
+}
